ea790a393a5c51a4b2dda39c63880aefcd24c3e6
rucka <miroslav.rucka@posam.sk>
2016-04-23 09:55:44 +0200

    correct parse date

--- src/sk/mirorucka/sensors/ISO8601.java
+++ src/sk/mirorucka/sensors/ISO8601.java
@@ -1,234 +1,26 @@
 package sk.mirorucka.sensors;
-// DateParser.java
-// $Id: DateParser.java,v 1.5 2005/05/16 10:19:19 ylafon Exp $
-// (c) COPYRIGHT MIT, INRIA and Keio, 2000.
-// Please first read the full copyright statement in file COPYRIGHT.html
 
-import java.util.Calendar;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.NoSuchElementException;
-import java.util.StringTokenizer;
 import java.util.TimeZone;
 
-/**
- * Date parser for ISO 8601 format
- * http://www.w3.org/TR/1998/NOTE-datetime-19980827
- *
- * @version $Revision: 1.5 $
- * @author Benot Mah (bmahe@w3.org)
- * @author Yves Lafon (ylafon@w3.org)
- */
 public class ISO8601 {
 
-    private static boolean check(StringTokenizer st, String token)
+    private static final DateFormat IN_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.S'Z'");
+    private static final DateFormat OUT_FORMAT = new SimpleDateFormat("HH:mm:ss");
 
-    {
-        try {
-            if (st.nextToken().equals(token)) {
-                return true;
-            } else {
-                throw new RuntimeException("Missing [" + token + "]");
-            }
-        } catch (NoSuchElementException ex) {
-            return false;
-        }
+    public static Date parse(String date) throws ParseException {
+        IN_FORMAT.setTimeZone(TimeZone.getTimeZone("UTC"));
+        return IN_FORMAT.parse(date);
     }
 
-    private static Calendar getCalendar(String isodate) {
-        // YYYY-MM-DDThh:mm:ss.sTZD
-        StringTokenizer st = new StringTokenizer(isodate, "-T:.+Z", true);
-
-        Calendar calendar = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
-        calendar.clear();
-        try {
-            // Year
-            if (st.hasMoreTokens()) {
-                int year = Integer.parseInt(st.nextToken());
-                calendar.set(Calendar.YEAR, year);
-            } else {
-                return calendar;
-            }
-            // Month
-            if (check(st, "-") && (st.hasMoreTokens())) {
-                int month = Integer.parseInt(st.nextToken()) - 1;
-                calendar.set(Calendar.MONTH, month);
-            } else {
-                return calendar;
-            }
-            // Day
-            if (check(st, "-") && (st.hasMoreTokens())) {
-                int day = Integer.parseInt(st.nextToken());
-                calendar.set(Calendar.DAY_OF_MONTH, day);
-            } else {
-                return calendar;
-            }
-            // Hour
-            if (check(st, "T") && (st.hasMoreTokens())) {
-                int hour = Integer.parseInt(st.nextToken());
-                calendar.set(Calendar.HOUR_OF_DAY, hour);
-            } else {
-                calendar.set(Calendar.HOUR_OF_DAY, 0);
-                calendar.set(Calendar.MINUTE, 0);
-                calendar.set(Calendar.SECOND, 0);
-                calendar.set(Calendar.MILLISECOND, 0);
-                return calendar;
-            }
-            // Minutes
-            if (check(st, ":") && (st.hasMoreTokens())) {
-                int minutes = Integer.parseInt(st.nextToken());
-                calendar.set(Calendar.MINUTE, minutes);
-            } else {
-                calendar.set(Calendar.MINUTE, 0);
-                calendar.set(Calendar.SECOND, 0);
-                calendar.set(Calendar.MILLISECOND, 0);
-                return calendar;
-            }
-
-            //
-            // Not mandatory now
-            //
-
-            // Secondes
-            if (!st.hasMoreTokens()) {
-                return calendar;
-            }
-            String tok = st.nextToken();
-            if (tok.equals(":")) { // secondes
-                if (st.hasMoreTokens()) {
-                    int secondes = Integer.parseInt(st.nextToken());
-                    calendar.set(Calendar.SECOND, secondes);
-                    if (!st.hasMoreTokens()) {
-                        return calendar;
-                    }
-                    // frac sec
-                    tok = st.nextToken();
-                    if (tok.equals(".")) {
-                        // bug fixed, thx to Martin Bottcher
-                        String nt = st.nextToken();
-                        while (nt.length() < 3) {
-                            nt += "0";
-                        }
-                        nt = nt.substring(0, 3); // Cut trailing chars..
-                        int millisec = Integer.parseInt(nt);
-                        // int millisec = Integer.parseInt(st.nextToken()) * 10;
-                        calendar.set(Calendar.MILLISECOND, millisec);
-                        if (!st.hasMoreTokens()) {
-                            return calendar;
-                        }
-                        tok = st.nextToken();
-                    } else {
-                        calendar.set(Calendar.MILLISECOND, 0);
-                    }
-                } else {
-                    throw new RuntimeException("No secondes specified");
-                }
-            } else {
-                calendar.set(Calendar.SECOND, 0);
-                calendar.set(Calendar.MILLISECOND, 0);
-            }
-            // Timezone
-            if (!tok.equals("Z")) { // UTC
-                if (!(tok.equals("+") || tok.equals("-"))) {
-                    throw new RuntimeException("only Z, + or - allowed");
-                }
-                boolean plus = tok.equals("+");
-                if (!st.hasMoreTokens()) {
-                    throw new RuntimeException("Missing hour field");
-                }
-                int tzhour = Integer.parseInt(st.nextToken());
-                int tzmin = 0;
-                if (check(st, ":") && (st.hasMoreTokens())) {
-                    tzmin = Integer.parseInt(st.nextToken());
-                } else {
-                    throw new RuntimeException("Missing minute field");
-                }
-                if (plus) {
-                    calendar.add(Calendar.HOUR, -tzhour);
-                    calendar.add(Calendar.MINUTE, -tzmin);
-                } else {
-                    calendar.add(Calendar.HOUR, tzhour);
-                    calendar.add(Calendar.MINUTE, tzmin);
-                }
-            }
-        } catch (NumberFormatException ex) {
-            throw new RuntimeException("[" + ex.getMessage() + "] is not an integer");
-        }
-        return calendar;
+    public static String convertISOtoTime(String date) throws ParseException {
+        OUT_FORMAT.setTimeZone(TimeZone.getTimeZone("Europe/Bratislava"));
+        return OUT_FORMAT.format(parse(date));
     }
 
-    /**
-     * Parse the given string in ISO 8601 format and build a Date object.
-     *
-     * @param isodate
-     *          the date in ISO 8601 format
-     * @return a Date instance
-     * @exception InvalidDateException
-     *              if the date is not valid
-     */
-    public static Date parse(String isodate) {
-        Calendar calendar = getCalendar(isodate);
-        return calendar.getTime();
-    }
 
-    private static String twoDigit(int i) {
-        if (i >= 0 && i < 10) {
-            return "0" + String.valueOf(i);
-        }
-        return String.valueOf(i);
-    }
-
-    /**
-     * Generate a ISO 8601 date
-     *
-     * @param date
-     *          a Date instance
-     * @return a string representing the date in the ISO 8601 format
-     */
-    public static String getIsoDate(Date date) {
-        Calendar calendar = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
-        calendar.setTime(date);
-        StringBuffer buffer = new StringBuffer();
-        buffer.append(calendar.get(Calendar.YEAR));
-        buffer.append("-");
-        buffer.append(twoDigit(calendar.get(Calendar.MONTH) + 1));
-        buffer.append("-");
-        buffer.append(twoDigit(calendar.get(Calendar.DAY_OF_MONTH)));
-        buffer.append("T");
-        buffer.append(twoDigit(calendar.get(Calendar.HOUR_OF_DAY)));
-        buffer.append(":");
-        buffer.append(twoDigit(calendar.get(Calendar.MINUTE)));
-        buffer.append(":");
-        buffer.append(twoDigit(calendar.get(Calendar.SECOND)));
-        buffer.append(".");
-        buffer.append(twoDigit(calendar.get(Calendar.MILLISECOND) / 10));
-        buffer.append("Z");
-        return buffer.toString();
-    }
-
-    /**
-     * Generate a ISO 8601 date
-     *
-     * @param date
-     *          a Date instance
-     * @return a string representing the date in the ISO 8601 format
-     */
-    public static String getIsoDateNoMillis(Date date) {
-        Calendar calendar = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
-        calendar.setTime(date);
-        StringBuffer buffer = new StringBuffer();
-        buffer.append(calendar.get(Calendar.YEAR));
-        buffer.append("-");
-        buffer.append(twoDigit(calendar.get(Calendar.MONTH) + 1));
-        buffer.append("-");
-        buffer.append(twoDigit(calendar.get(Calendar.DAY_OF_MONTH)));
-        buffer.append("T");
-        buffer.append(twoDigit(calendar.get(Calendar.HOUR_OF_DAY)));
-        buffer.append(":");
-        buffer.append(twoDigit(calendar.get(Calendar.MINUTE)));
-        buffer.append(":");
-        buffer.append(twoDigit(calendar.get(Calendar.SECOND)));
-        buffer.append("Z");
-        return buffer.toString();
-    }
 }
--- src/sk/mirorucka/sensors/UpdateWidgetService.java
+++ src/sk/mirorucka/sensors/UpdateWidgetService.java
@@ -17,14 +17,17 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import java.io.InputStream;
 import java.text.DateFormat;
 import java.text.DecimalFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
 public class UpdateWidgetService extends Service {
 
     private static final DecimalFormat DF = new DecimalFormat("0.00##");
-    private static final DateFormat IN_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SZ", Locale.ENGLISH);
+    private static final DateFormat IN_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.S'Z'", Locale.ENGLISH);
     private static final DateFormat OUT_FORMAT = new SimpleDateFormat("HH:mm:ss", Locale.ENGLISH);
+
+    //2016-03-22T05:24:19.168Z
 
     @Override
     public void onStart(Intent intent, int startId) {
@@ -43,9 +46,9 @@ public class UpdateWidgetService extends Service {
                 remoteViews.setTextViewText(R.id.pressure, "Tlak: " + DF.format(first.getPressure()) + " hPa");
                 remoteViews.setTextViewText(R.id.humidity, "Vlhkosť: " + DF.format(first.getHumidity()) + " %");
                 remoteViews.setTextViewText(R.id.light, "Svetlo: " + DF.format(first.getLight()) + " lx");
-                remoteViews.setTextViewText(R.id.temperatureIn, "Von: " + DF.format(first.getTemperature().get(0).getValue()) + " °C");
-                remoteViews.setTextViewText(R.id.temperatureOut, "Dnu: " + DF.format(first.getTemperature().get(1).getValue()) + " °C");
-                remoteViews.setTextViewText(R.id.updated, "updated: " + OUT_FORMAT.format(ISO8601.parse(first.getTimestamp())));
+                remoteViews.setTextViewText(R.id.temperatureIn, "Von: " + DF.format(first.getTemperature().get(1).getValue()) + " °C");
+                remoteViews.setTextViewText(R.id.temperatureOut, "Dnu: " + DF.format(first.getTemperature().get(2).getValue()) + " °C");
+                remoteViews.setTextViewText(R.id.updated, "updated: " + ISO8601.convertISOtoTime(first.getTimestamp()));
             } catch (Exception e) {
                 Log.e(Constant.APP_TAG.getCode(), e.getMessage(), e);
                 remoteViews.setTextViewText(R.id.updated, "updated: error!");
@@ -59,17 +62,5 @@ public class UpdateWidgetService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return null;
-    }
-
-    public SensorsData[] postResetRequest(String endpoint) throws Exception {
-        HttpClient httpclient = new DefaultHttpClient(HttpUtils.getHttpClientSettings(this));
-        HttpGet httpget = new HttpGet(endpoint + "/api/sensors/last");
-        httpget.setHeader("Accept", "application/json");
-        httpget.setHeader("Content-type", "application/json");
-        httpget.setHeader("Authorization", "c3VzbGlrOmJ1Ym8=");
-        HttpResponse httpResponse = httpclient.execute(httpget);
-        httpclient.getConnectionManager().shutdown();
-        InputStream inputStream = httpResponse.getEntity().getContent();
-        return new Gson().fromJson(IOUtils.toString(inputStream), SensorsData[].class);
     }
 }
